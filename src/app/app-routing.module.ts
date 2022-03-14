e6e3ac3cd6beeb95973fcf7f4066667fcbbe0cf1
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-03-14 23:32:50 +0530

    feat page module initiated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'error', loadChildren: () => import('./pages/error/error.module').then(m => m.ErrorModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
