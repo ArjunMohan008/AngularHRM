c7b770bb1dbbe99a6fee2aa7cf8df5137adaddd5
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-03-15 21:23:40 +0530

    faet add/edit departments

--- src/app/features/departments/departments-routing.module.ts
+++ src/app/features/departments/departments-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddEditDepartmentComponent } from './components/add-edit-department/add-edit-department.component';
 
 import { DepartmentsComponent } from './departments.component';
 
-const routes: Routes = [{ path: '', component: DepartmentsComponent }];
+const routes: Routes = [{ path: '', component: AddEditDepartmentComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/departments/departments.module.ts
+++ src/app/features/departments/departments.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { DepartmentsRoutingModule } from './departments-routing.module';
 import { DepartmentsComponent } from './departments.component';
+import { AddEditDepartmentComponent } from './components/add-edit-department/add-edit-department.component';
 
 
 @NgModule({
-  declarations: [DepartmentsComponent],
+  declarations: [DepartmentsComponent, AddEditDepartmentComponent],
   imports: [
     CommonModule,
     DepartmentsRoutingModule
