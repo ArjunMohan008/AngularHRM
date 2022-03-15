d65187db7dd6fde620eb71966afc922f8d5e02da
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-03-15 22:52:51 +0530

    style add/edit departments changes

--- src/app/features/departments/components/add-edit-department/add-edit-department.component.ts
+++ src/app/features/departments/components/add-edit-department/add-edit-department.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-edit-department',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-department.component.scss']
 })
 export class AddEditDepartmentComponent implements OnInit {
+  isEditing: boolean = false;
 
-  constructor() { }
+  departmentForm = this.fb.group({
+    name: [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
+    description: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(8)])],
+    manager: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(8)])],
+  });
+
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    if (this.departmentForm.valid) {
+      console.table(this.departmentForm.value);
+
+      this.departmentForm.reset();
+    }
+  }
+
+  get departmentFormFormControl() {
+    return this.departmentForm.controls;
+  }
+
+  goToDepartmentList() {
+    this.router.navigateByUrl('/departments');
+  }
+
+  onReset() {
+    this.departmentForm.reset();
+  }
+
+  onCancel() {
+    console.log('Cancelled');
   }
 
 }
