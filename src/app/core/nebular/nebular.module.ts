2fb5acb6e82ab59d234fab99a2f1b93326af0624
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-03-15 21:32:06 +0530

    feat nabular module created

---
+++ src/app/core/nebular/nebular.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbProgressBarModule,
+  NbTabsetModule,
+  NbUserModule,
+  NbIconModule,
+  NbSelectModule,
+  NbListModule,
+  NbMenuModule,
+} from '@nebular/theme';
+
+const mm = [
+  CommonModule,
+  NbButtonModule,
+  NbCardModule,
+  NbProgressBarModule,
+  NbTabsetModule,
+  NbUserModule,
+  NbIconModule,
+  NbSelectModule,
+  NbListModule,
+  NbMenuModule,
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ...mm
+  ],
+  exports: mm
+})
+export class NebularModule { }
