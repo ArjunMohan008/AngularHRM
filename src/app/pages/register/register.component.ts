db67e1490d9adf3a592b6791d584fe918bb33510
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-03-16 01:22:34 +0530

    feat register/login page layout

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm = this.fb.group({
+   firstName: [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
+    lastName: [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
+    email: [null, Validators.compose([Validators.required, Validators.email])],
+    password: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(8)])],
+    confirmPassword: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(8)])],
+  });
 
-  constructor() { }
+
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.router.navigateByUrl('/dashboard');
+
+      console.table(this.registerForm.value);
+
+      this.registerForm.reset();
+    }
+  }
+
+  get registerFormFormControl() {
+    return this.registerForm.controls;
+  }
+
+  goToSignup() {
+    this.router.navigateByUrl('/register');
+  }
+
+  goToForgotPassword() {
+    this.router.navigateByUrl('/forgot-password');
   }
 
 }
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
+import { CoreModule } from 'src/app/core/core.module';
+
 
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    CoreModule
   ]
 })
 export class RegisterModule { }
