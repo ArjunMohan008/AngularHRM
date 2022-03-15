803c2f46465e3f4087676bd85c5efd8def896426
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-03-15 18:00:35 +0530

    feat departments module created

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -3,7 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      { path: 'home', component: DashboardComponent },
+      { path: 'departments', loadChildren: () => import('../../features/departments/departments.module').then(m => m.DepartmentsModule) },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
