d5c8f4633a6e795efa9e1a19a5d643e41c0bc8b7
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-03-16 08:23:26 +0530

    feat home route to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
   { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
